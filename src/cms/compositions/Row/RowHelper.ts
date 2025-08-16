f88fe70f0a493dd629bb7d109dc86e4e608af66d
Kunal Shetye <kunal.shetye@gmail.com>
2025-08-16 14:21:29 +0200

    updated row helper

--- src/cms/compositions/Row/RowHelper.ts
+++ src/cms/compositions/Row/RowHelper.ts
@@ -42,7 +42,28 @@ export function getRowStyles(row: CompositionStructureNode) {
         none = 'my-0',
     }
 
+    enum RowWidthClasses {
+        full = 'w-full',
+        container = 'container mx-auto px-8',
+        max7xl = 'max-w-7xl w-full mx-auto px-8',
+        max6xl = 'max-w-6xl w-full mx-auto px-8',
+        max5xl = 'max-w-5xl w-full mx-auto px-8',
+        max4xl = 'max-w-4xl w-full mx-auto px-8',
+        max3xl = 'max-w-3xl w-full mx-auto px-8',
+        max2xl = 'max-w-2xl w-full mx-auto px-8',
+        maxXl = 'max-w-xl w-full mx-auto px-8',
+        maxLg = 'max-w-lg w-full mx-auto px-8',
+        maxMd = 'max-w-md w-full mx-auto px-8',
+        maxSm = 'max-w-sm w-full mx-auto px-8',
+        maxXs = 'max-w-xs w-full mx-auto px-8',
+        inherit = '', // No additional width classes, inherit from parent
+    }
+
     let cssClasses = [];
+    // Apply width classes if specified, otherwise inherit from section
+    if (dictionary['rowWidth'] && dictionary['rowWidth'] !== 'inherit') {
+        cssClasses.push(RowWidthClasses[dictionary['rowWidth']] ?? '');
+    }
     cssClasses.push(ContentSpacingClasses[dictionary['contentSpacing']] ?? '');
     cssClasses.push(JustifyContentClasses[dictionary['justifyContent']] ?? '');
     cssClasses.push(AlignContentClasses[dictionary['alignContent']] ?? '');
